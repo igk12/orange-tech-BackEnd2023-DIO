2f17811ca87cfd484ac813674a4eb08f00ddbffc
igk12 <91812991+igk12@users.noreply.github.com>
2023-01-14 19:41:33 -0300

    Envio projeto SmartTv

---
+++ smart-tv-aprendendo-sintaxe-java/src/SmartTv.java
@@ -0,0 +1,38 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 17;
+
+    public void ligar(){
+        ligada = true;
+    }
+
+    public void desligar(){
+        ligada = false;
+    }
+
+    public void aumentarVolume() {
+        volume++;
+        System.out.println("Aumentando Volume para " + volume);
+        
+    }
+
+    public void diminuirVolume() {
+        volume--;
+        System.out.println("Diminuindo Volume para " + volume);
+    }
+
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+
+    }
+
+    public void aumentarCanal(){
+        canal++;
+    }
+
+    public void diminuirCanal(){
+        canal--;
+    }
+
+}
