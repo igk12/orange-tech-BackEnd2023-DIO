5361fb84176e2d3c449b369d5ec60aa7d4191f8e
igk12 <91812991+igk12@users.noreply.github.com>
2023-01-14 21:11:27 -0300

    exemplo 1 - Aula operadores logicos - logica condicional

--- operadores-logicos--logica-condicional-controle-de-fluxo/src/ExemploUm.java
+++ operadores-logicos--logica-condicional-controle-de-fluxo/src/ExemploUm.java
@@ -5,33 +5,25 @@ public class ExemploUm {
         boolean b2 = false;
         boolean b3 = true;
         boolean b4 = false;
-        int i1 = 3;
+        int i1 = 0;
         int i2 = 2;
         float f1 = 1.1f;
-        float f2 = 1.2f;
+        float f2 = 3.2f;
 
-        boolean resultado = b1&&b2;
-        boolean resultado2 = b1&&b3;
-        System.out.println("b1&&b2 = " + resultado + " b1&&b3 = " + resultado2);
 
-        boolean resultado3 = b2||b3;
-        boolean resultado4 = b2||b4;
-        System.out.println("b2||b3 = " + resultado3 + " b2||b4 = " + resultado4);
+        System.out.println("b1&&b2 = " + (b1&&b2) + " ----------- b1&&b3 = " + (b1&&b3));
 
-        boolean resultado5 = b1^b3;
-        boolean resultado6 = b4^b1;
-        System.out.println("b1^b3 = " + resultado5 + " b4^b1 = " + resultado6);
+        System.out.println("b2||b3 = " + (b2||b3) + " ----------- b2||b4 = " + (b2||b4));
 
-        boolean resultado7 = !b1;
-        boolean resultado8 = !b2;
-        System.out.println("!b1 = " + resultado7 + " !b2 = " + resultado8);
+        System.out.println("b1^b3 = " + (b1^b3) + " ----------- b4^b1 = " + (b4^b1));
+
+        System.out.println("!b1 = " + !b1 + " ----------- !b2 = " + !b2);
 
         boolean calculo1 =  (f2 < f1);
         boolean calculo2 = (i1 > i2);
-        boolean resultado10 = calculo1 || calculo2;
-        System.out.println("(i1 > i2) || (f2 < f1) = " + resultado10);
+        System.out.println("(i1 > i2) || (f2 < f1) = " + (calculo1 || calculo2));
 
         //boolean resultado = ((i1 + i2)<(f2 - f1)) && true;
-        //System.out.println("b1((i1 + i2)<(f2 - f1)) && true = " + resultado);
+        System.out.println("b1((i1 + i2)<(f2 - f1)) && true = " + (((i1 + i2)<(f2 - f1)) && true));
     }
 }
